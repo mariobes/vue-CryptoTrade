f429241eb73934dc6f45ee1d7b1654c603a91be5
MARIO\mario <mariobes01@gmail.com>
2025-04-28 15:59:38 +0200

    Añadir y maquetar Transaction

---
+++ src/core/user.ts
@@ -0,0 +1,18 @@
+import type { Transaction } from "./transaction"
+
+export interface User {
+    id: number
+    name: string
+    birthday: Date
+    email: string
+    password: string
+    phone: string
+    dni: string
+    nationality: string
+    cash: number
+    wallet: number
+    profit: number
+    creationDate: Date
+    transactions: Transaction[]
+    role: string
+}
