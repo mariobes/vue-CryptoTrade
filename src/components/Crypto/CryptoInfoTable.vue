a02db091714c183b8ba56584b5f7e2a12347312e
MARIO\mario <mariobes01@gmail.com>
2025-05-22 18:29:52 +0200

    Solucionar fallos de código 10

--- src/components/Crypto/CryptoInfoTable.vue
+++ src/components/Crypto/CryptoInfoTable.vue
@@ -295,10 +295,10 @@ onMounted(() => {
 			<div class="historical-data-content">
 				<div class="historical-data-text">
 					<span class="historical-data-title">{{ t('CryptoInfoTable_Ath') }}</span>
-					<span class="historical-data-date">{{ crypto.ath_date ? formatDate(String(crypto.ath_date)) : '0' }}</span>
+					<span class="historical-data-date">{{ crypto.ath_date ? formatDate(String(crypto.ath_date)) : '---' }}</span>
 				</div>
 				<div class="historical-data-price">
-					<span>{{ storeUserPreferences.convertPrice(crypto.ath, storeUserPreferences.selectedCurrency, 'before') }}</span>
+					<span>{{ crypto.ath ? storeUserPreferences.convertPrice(crypto.ath, storeUserPreferences.selectedCurrency, 'before') : '0' }}</span>
 					<span :style="{ color: storeUserPreferences.getPriceColor(crypto.ath_change_percentage) }">
 						{{ crypto.ath_change_percentage ? crypto.ath_change_percentage.toFixed(2) : '0' }}%
 					</span>
@@ -308,10 +308,10 @@ onMounted(() => {
 			<div class="historical-data-content">
 				<div class="historical-data-text">
 					<span class="historical-data-title">{{ t('CryptoInfoTable_Atl') }}</span>
-					<span class="historical-data-date">{{ crypto.atl_date ? formatDate(String(crypto.atl_date)) : '0' }}</span>
+					<span class="historical-data-date">{{ crypto.atl_date ? formatDate(String(crypto.atl_date)) : '---' }}</span>
 				</div>
 				<div>
-					<span class="historical-data-price">{{ storeUserPreferences.convertPrice(crypto.atl, storeUserPreferences.selectedCurrency, 'before') }}</span>
+					<span class="historical-data-price">{{ crypto.atl ? storeUserPreferences.convertPrice(crypto.atl, storeUserPreferences.selectedCurrency, 'before') : '0' }}</span>
 					<span :style="{ color: storeUserPreferences.getPriceColor(crypto.atl_change_percentage) }">
 						{{ crypto.atl_change_percentage > 0 ? '+' : '' }}{{ crypto.atl_change_percentage ? (crypto.atl_change_percentage ?? 0).toFixed(2) : '0' }}%
 					</span>
